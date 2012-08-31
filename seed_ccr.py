419aadfb7a3fd878eb431a896d79293a63b4cf6a
Eric Wolf <ebwolf@gmail.com>
2012-08-31 14:34:26 -0600

    Made index.html generic. Pointed seed_ccr at navigator.

--- seed_ccr.py
+++ seed_ccr.py
@@ -3,7 +3,7 @@
 import os, sys, urllib2
 from TileMath import WGS84toTileXY
 
-for zoom in range(1,18):
+for zoom in range(5,20):
   if zoom < 8:
     xstart = 0
     ystart = 0
@@ -14,13 +14,13 @@ for zoom in range(1,18):
     #(xstart, ystart) = WGS84toTileXY(89, -179, zoom)
   elif zoom > 8 and zoom <= 12:
     # Contig US
-    (xend, yend) = WGS84toTileXY(24, -125, zoom)
+    (xend, yend) = WGS84toTileXY(23, -125, zoom)
     (xstart, ystart) = WGS84toTileXY(50, -66, zoom)
   else:
     # Colorado
     #(xend, yend) = WGS84toTileXY(37, -102, zoom)
-    (xend, yend) = WGS84toTileXY(37, -105.3, zoom)
-    (xstart, ystart) = WGS84toTileXY(41, -109.6, zoom)
+    (xend, yend) = WGS84toTileXY(36, -105, zoom)
+    (xstart, ystart) = WGS84toTileXY(42, -110, zoom)
 
   print "Zoom: " + str(zoom)
   print "Start: " + str(xstart) + ", " + str(ystart)
@@ -28,7 +28,7 @@ for zoom in range(1,18):
 
   for x in range(xstart, xend + 1):
     for y in range(ystart, yend + 1):
-      url = 'http://navtc.er.usgs.gov/tiles/ccr.cgi/' + str(zoom) + '/' + str(x) + '/' + str(y)
+      url = 'http://navigator.er.usgs.gov/tiles/ccr.cgi/' + str(zoom) + '/' + str(x) + '/' + str(y)
 
       print "Get " + url      
       try:
